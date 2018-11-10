80670ba1542d25c448c16714596e981fa631d0dc
AEJester <ryanmon731@gmail.com>
2018-11-10 15:37:54 +0000

    Made adjustments to the CLI list command.

--- dist/src/list.js
+++ dist/src/list.js
@@ -2,7 +2,11 @@ const fs = require("fs");
 
 const list = () => {
     let items = fs.readdirSync("./database/");
-    return items;
+    let finalized = [];
+    for (let i = 0; i < items.length; i++) {
+        finalized.push(items[i].replace(".json", ""));
+    }
+    return finalized;
 }
 
 module.exports = list;
