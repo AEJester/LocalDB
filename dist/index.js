9b6b3d8856aaae6a33de2ed8873d30598b2c457e
AEJester <ryanmon731@gmail.com>
2018-11-10 03:51:23 +0000

    MAde the remove function available for scripting.

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,14 @@ const create = require("./src/create");
 const store = require("./src/store");
 const init = require("./src/init");
 const get = require("./src/get");
+const remove = require("./src/remove");
 
 const lib = {
     create: create,
     store: store,
     init: init,
-    get: get
+    get: get,
+    remove: remove
 };
 
 module.exports = lib;
