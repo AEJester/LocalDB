7cc072599605b05878c53ab130cd678cfea72752
AEJester <ryanmon731@gmail.com>
2018-11-10 08:32:00 -0500

    Updated docs for 2 new methods.

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
 let datachest = require("../dist/index");
 
-let x = datachest.search("name", ["passwords", "usernames", "emails"]);
+let x = datachest.search("name", ["passwords", "usernames"]);
 console.log(x);
